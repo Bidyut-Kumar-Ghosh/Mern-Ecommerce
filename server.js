0d4831def7b66299eec51e515776a060ee86d803
Bidyut <bidyutghoshoffice@yahoo.com>
2023-05-09 00:26:18 +0530

    add ;

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
+const express = require('express');
 const res = require('express/lib/response')
 
 //rest object
-const app = express()
+const app = express();
 
 //rest api
 app.get('/',(req,res) => {
     res.send({
-        message:"Welcome to my ecommerse app"
-    })
-})
+        message:"Welcome to my ecommerse app",
+    });
+});
 
 //PORT
 const port = 8080
 
 //run listen
 app.listen(PORT,() =>{
-    console.log(`server running on ${PORT}`)
+    console.log(`server running on ${PORT}`);
 })
