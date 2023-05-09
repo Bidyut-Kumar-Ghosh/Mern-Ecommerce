ab45010e4f71fb3311848aa944dac4bbd5b3ee54
Bidyut <bidyutghoshoffice@yahoo.com>
2023-05-09 21:06:44 +0530

    email password verification and validation done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helper/authHelper.js";
+import { comparePassword, hashPassword } from "../helper/authHelper.js";
+import  JWT from "jsonwebtoken";
 
 export const registerController = async(req,res) => {
     try {
@@ -57,3 +58,53 @@ export const registerController = async(req,res) => {
     }
 };
 
+//post login 
+export const loginController = async (req,res) => {
+    try{
+        const {email,password} = req.body
+        // validation
+
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:'Invalid email or password'
+            })
+        }
+        //check if user exist
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message: 'Email is not registered'
+            })
+        }
+        const match = await comparePassword(password,user.password)
+        if(!match){
+            return res.status(200).send({
+                sucess:false,
+                message:'Invalid password'
+            })
+
+        }
+        //token
+        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+        res.status(200).send({
+            success:true,
+            message:'login successfuly',
+            user:{
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token, 
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in login',
+            error
+        })
+    }
+}
