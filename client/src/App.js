48e0078e531b5b74bb3d8f8c0d82972608bd738d
Bidyut <bidyutghoshoffice@yahoo.com>
2023-05-11 19:26:51 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
     <>
-      <h1>Ecommerce App</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </>
   );
 }
