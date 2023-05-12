d5a5e1229c4107cc8ac240185689b5a020b5e8b4
Bidyut <bidyutghoshoffice@yahoo.com>
2023-05-12 12:17:35 +0530

    footer create

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <div className='bg-dark text-light p-3'>
+        <div className='footer'>
             <h1 className='text-center'>
                 All Rights Reserved &copy; Bidyut
             </h1>
+            <p className='text-center mt-3'>
+                <Link to="/about">About</Link>
+                <Link to="/Contact">Contact us</Link>
+                <Link to="/Policy">Privacy Policy</Link>
+            </p>
         </div>
     )
 }
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import { GiShoppingBag } from 'react-icons/gi'
 
 const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" href="#">Navbar</NavLink>
+                    <Link className="navbar-brand">
+                        <GiShoppingBag /> ecommmerce APP</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink >
+                                <NavLink to="/" className="nav-link">Home</NavLink >
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link" href="#">Link</NavLink >
+                                <NavLink to="/category" className="nav-link">Category</NavLink >
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link disabled">Disabled</NavLink >
+                                <NavLink to="/register" className="nav-link">
+                                    Register</NavLink >
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link">
+                                    Login</NavLink >
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/cart" className="nav-link">
+                                    Cart (0)</NavLink >
                             </li>
                         </ul>
                     </div>
