7e38a2126c0a65a8b72882fc4047cc08e55e3d48
Bidyut <deepghosh369@gmail.com>
2023-05-13 20:00:40 +0530

    added phone validation in register

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -18,45 +18,57 @@ const Register = () => {
                         <input
                             type="text"
                             value={name}
+                            onChange={(e) => setName(e.target.value)}
                             className="form-control"
                             id="exampleInputEmail1"
                             placeholder='Enter Your Name'
+                            required
                         />
                     </div>
                     <div className="mb-3">
                         <input
                             type="email"
                             value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className="form-control"
                             id="exampleInputEmail1"
                             placeholder='Email Address'
+                            required
                         />
                     </div>
                     <div className="mb-3">
+
                         <input
-                            type="num"
+                            type="tel"
                             value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
                             className="form-control"
                             id="exampleInputEmail1"
                             placeholder='Phone'
+                            pattern="[0-9]{3}[0-9]{3}[0-9]{3}[0-9]{1}"
+                            required
                         />
                     </div>
                     <div className="mb-3">
                         <input
                             type="password"
                             value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="form-control"
                             id="exampleInputPassword1"
                             placeholder='Password'
+                            required
                         />
                     </div>
                     <div className="mb-3">
                         <input
                             type="text"
                             value={address}
+                            onChange={(e) => setAddress(e.target.value)}
                             className="form-control"
                             id="exampleInputEmail1"
                             placeholder='Address'
+                            required
                         />
                     </div>
                     {/* <div className="mb-3 form-check">
