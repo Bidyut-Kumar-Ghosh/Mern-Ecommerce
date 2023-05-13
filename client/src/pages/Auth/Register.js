2e0978926ae46716fa400a5704667d63f8eeecab
Bidyut <bidyutghoshoffice@yahoo.com>
2023-05-13 12:42:31 +0530

    add values

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 
 const Register = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+    const [address, setAddress] = useState("")
+
+
     return (
         <Layout title="Register Ecommerce">
             <div className="register">
                 <h1>Register Page</h1>
                 <form>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputName" className="form-label">
-                            Name
-                        </label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" />
+                        <input
+                            type="text"
+                            value={name}
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            placeholder='Enter Your Name'
+                        />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail" className="form-label">
-                            Email Address
-                        </label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" />
+                        <input
+                            type="email"
+                            value={email}
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            placeholder='Email Address'
+                        />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputName" className="form-label">
-                            Phone
-                        </label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" />
+                        <input
+                            type="num"
+                            value={phone}
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            placeholder='Phone'
+                        />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <input
+                            type="password"
+                            value={password}
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder='Password'
+                        />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputName" className="form-label">
-                            Address
-                        </label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" />
+                        <input
+                            type="text"
+                            value={address}
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            placeholder='Address'
+                        />
                     </div>
                     {/* <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
