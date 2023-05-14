097913b85226695186446f876f3ea4444329721b
Bidyut <deepghosh369@gmail.com>
2023-05-15 01:19:08 +0530

    changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import { useAuth } from '../context/auth'
 
 
 const HomePage = () => {
-    const [auth, setAuth] = useAuth()
+    const [auth,] = useAuth()
     return (
         <Layout>
             <h1>HomePage</h1>
