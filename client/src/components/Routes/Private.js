c37e5261c66365b5fd27146ccd0cc0d8f6000510
Bidyut <deepghosh369@gmail.com>
2023-05-15 13:00:03 +0530

    spinner add

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -6,17 +6,11 @@ import Spinner from "../Spinner";
 
 export default function PrivateRoute() {
     const [ok, setOk] = useState(false);
-    const [auth, setAuth] = useAuth();
+    const [auth,] = useAuth();
 
     useEffect(() => {
         const authCheck = async () => {
-            const res = await axios.get("/api/v1/auth/user-auth",
-                {
-                    headers: {
-                        "Authorization": auth?.token
-                    }
-                }
-            )
+            const res = await axios.get("/api/v1/auth/user-auth");
             if (res.data.ok) {
                 setOk(true);
             } else {
