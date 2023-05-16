612cff0b17c46f9cb9b909d79a93a2e37064ec16
Bidyut <deepghosh369@gmail.com>
2023-05-16 22:13:31 +0530

    added some new changes in navlink

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -46,14 +46,28 @@ const Header = () => {
                                             <NavLink to="/login" className="nav-link">
                                                 Login</NavLink >
                                         </li>
-                                    </>)
-                                    :
-                                    (<>
-                                        <li className="nav-item">
-                                            <NavLink onClick={handleLogout} to="/login" className="nav-link">
-                                                Log Out</NavLink >
-                                        </li>
-                                    </>)
+                                    </>
+                                    ) : (
+                                        <>
+                                            <li className="nav-item dropdown">
+                                                <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    {auth?.user?.name}
+                                                </NavLink>
+                                                <ul className="dropdown-menu">
+                                                    <li><NavLink to="/dashboard" className="dropdown-item" href="#">Dashboard</NavLink></li>
+                                                    <li>
+                                                        <NavLink onClick={handleLogout} to="/login" className="dropdown-item">
+                                                            Log Out</NavLink >
+                                                    </li>
+
+                                                </ul>
+                                            </li>
+
+                                            {/* <li className="nav-item">
+                                                <NavLink onClick={handleLogout} to="/login" className="nav-link">
+                                                    Log Out</NavLink >
+                                            </li> */}
+                                        </>)
                             }
 
                             <li className="nav-item">
