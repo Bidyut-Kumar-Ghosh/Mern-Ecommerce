e4be13f1d3607d5912c08d9322441ea409aa0c62
Bidyut <deepghosh369@gmail.com>
2023-05-17 12:33:28 +0530

    changes new

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
 
-export default function PrivateRoute() {
+export default function AdminRoute() {
     const [ok, setOk] = useState(false);
-    const [auth, setAuth] = useAuth();
+    const [auth] = useAuth();
 
     useEffect(() => {
         const authCheck = async () => {
