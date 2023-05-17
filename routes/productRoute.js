850623be9ec59554efdbc9205f044d57aa789144
Bidyut <deepghosh369@gmail.com>
2023-05-17 21:52:30 +0530

    new added product ,category routes and auth

---
+++ routes/productRoute.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
+import { createProductController, getProductController, getSingleProductController } from '../controllers/productController.js'
+import formidable from 'express-formidable'
+
+const router = express.Router()
+
+
+//routes
+router.post(
+    '/create-product',
+    requireSignIn,
+    isAdmin,
+    formidable(),
+    createProductController
+);
+
+//Get Products 
+router.get('/get-product', getProductController)
+
+
+
+//single product
+router.get('/get-product/:slug', getSingleProductController)
+
+
+
+export default router;
