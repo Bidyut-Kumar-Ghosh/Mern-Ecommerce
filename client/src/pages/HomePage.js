d959fa692186adda534e281b2d862a9b2d761f60
Bidyut Kumar Ghosh <deepghosh369@gmail.com>
2023-05-23 13:59:55 +0530

    filter by price and category done

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -49,81 +49,108 @@ const HomePage = () => {
 
     // filter by cat
     const handleFilter = (value, id) => {
-        let all = [...checked]
+        let all = [...checked];
         if (value) {
             all.push(id);
-        }
-        else {
+        } else {
             all = all.filter((c) => c !== id);
         }
-        setChecked(all)
-    }
-
+        setChecked(all);
+    };
     useEffect(() => {
         if (!checked.length || !radio.length) getAllProducts();
-
     }, [checked.length, radio.length]);
 
     useEffect(() => {
         if (checked.length || radio.length) filterProduct();
     }, [checked, radio]);
 
-
-    //get filtered product
+    //get filterd product
     const filterProduct = async () => {
         try {
-            const { data } = await axios.post('/api/v1/product/product-flters', { checked, radio })
-            setProducts(data?.products)
+            const { data } = await axios.post("/api/v1/product/product-filters", {
+                checked,
+                radio,
+            });
+            setProducts(data?.products);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
     return (
-        <Layout title={"All Products - Best Offers"}>
-            <div className='row mt-3'>
-                <div className='col-md-2'>
-                    <h4 className='text-center'>Filter By Category</h4>
-                    <div className='d-flex flex-column'>
-                        {categories?.map(c => (
-                            <Checkbox key={c._id} onChange={(e) => handleFilter(e.target.checked, c._id)}>
+        <Layout title={"ALl Products - Best offers "}>
+            <div className="container-fluid row mt-3">
+                <div className="col-md-2">
+                    <h4 className="text-center">Filter By Category</h4>
+                    <div className="d-flex flex-column">
+                        {categories?.map((c) => (
+                            <Checkbox
+                                key={c._id}
+                                onChange={(e) => handleFilter(e.target.checked, c._id)}
+                            >
                                 {c.name}
                             </Checkbox>
                         ))}
                     </div>
-                    {/* Price Filter */}
-                    <h4 className='text-center mt-4'>Filter By Price</h4>
-                    <div className='d-flex flex-column'>
-                        <Radio.Group onChange={e => setRadio(e.target.value)}>
-                            {Prices?.map(p => (
+                    {/* price filter */}
+                    <h4 className="text-center mt-4">Filter By Price</h4>
+                    <div className="d-flex flex-column">
+                        <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                            {Prices?.map((p) => (
                                 <div key={p._id}>
                                     <Radio value={p.array}>{p.name}</Radio>
                                 </div>
                             ))}
                         </Radio.Group>
                     </div>
+                    <div className="d-flex flex-column">
+                        <button
+                            className="btn btn-danger"
+                            onClick={() => window.location.reload()}
+                        >
+                            RESET FILTERS
+                        </button>
+                    </div>
                 </div>
-                <div className='col-md-9'>
-                    {JSON.stringify(radio, null, 4)}
-                    <h1 className='text-center'>All Products</h1>
-                    <div className='d-flex flex-wrap'>
+                <div className="col-md-9">
+                    <h1 className="text-center">All Products</h1>
+                    <div className="d-flex flex-wrap">
                         {products?.map((p) => (
-                            <div className="card m-2" style={{ width: '18rem' }} >
-                                <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} />
+                            <div className="card m-2" style={{ width: "18rem" }}>
+                                <img
+                                    src={`/api/v1/product/product-photo/${p._id}`}
+                                    className="card-img-top"
+                                    alt={p.name}
+                                />
                                 <div className="card-body">
                                     <h5 className="card-title">{p.name}</h5>
-                                    <p className="card-text">{p.description.substring(0, 30)}...</p>
+                                    <p className="card-text">
+                                        {p.description.substring(0, 30)}...
+                                    </p>
                                     <p className="card-text"> $ {p.price}</p>
                                     <button class="btn btn-primary ms-1">More Details</button>
-                                    <button class="btn btn-danger ms-1">Add to Cart</button>
+                                    <button class="btn btn-secondary ms-1">ADD TO CART</button>
                                 </div>
                             </div>
                         ))}
                     </div>
+                    {/* <div className="m-2 p-3">
+                        {products && products.length < total && (
+                            <button
+                                className="btn btn-warning"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setPage(page + 1);
+                                }}
+                            >
+                                {loading ? "Loading ..." : "Loadmore"}
+                            </button>
+                        )}
+                    </div> */}
                 </div>
             </div>
-
         </Layout>
-    )
-}
+    );
+};
 
 export default HomePage;
